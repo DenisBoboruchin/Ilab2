901906eb546e64445f5efce955df986fbe90f932
Denis <denis.boboruchin@gmail.com>
2022-09-09 12:02:43 +0300

    start LRU

--- cashe/include/cashe.h
+++ cashe/include/cashe.h
@@ -9,25 +9,38 @@ namespace cashe
 template <typename T, typename keyT = int> struct cashe_t 
 {
 private:
-    struct elem_
-    {
-        T   value;
-        int num = 0;                                        // addition number  
-    };
-
     size_t  sz_     = 0;
-    int     nadd_   = 0;
+    
+    std::list <T> cashe_;
 
-    using ListPtr = typename std::list<elem_>::pointer;
+    using ListPtr = typename std::list <T>::pointer;
     std::unordered_map <keyT, ListPtr> hash_;
 
 public:
-    cashe_t (size_t sz): sz_ {sz}, nadd_ {0} {};
+    cashe_t (size_t sz): sz_ {sz} {};
+   
+    bool check_full_ () const 
+    {
+        size_t csize = cashe_.size;
+        assert (csize == hash_.size); 
+
+        return (csize == sz_);
+    }
 
     template <typename F> bool lookup_update (keyT key, F slow_get_page)
     {
-        
+        auto hit = hash_.find (key);
+        if (hit == hash_.end ())
+        {
+            if (check_full_ ())
+            {
+                hash_.erase ();
+            }
 
+            return true;
+        }
+
+        
         return true;
     }
 };
--- cashe/src/cashe.cpp
+++ cashe/src/cashe.cpp
@@ -4,4 +4,3 @@ int slow_get_page_int (int key)
 {
     return key;
 }
-
