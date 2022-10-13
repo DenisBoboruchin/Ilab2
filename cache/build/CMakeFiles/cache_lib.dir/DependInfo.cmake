4cc3dde5232a7a151ea29f87ac043fa8929c7fb1
Denis <denis.boboruchin@gmail.com>
2022-10-13 23:53:13 +0300

    caches don't work........

---
+++ cache/build/CMakeFiles/cache_lib.dir/DependInfo.cmake
@@ -0,0 +1,19 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/denis/prog/Ilab2/cache/src/cache.cpp" "CMakeFiles/cache_lib.dir/src/cache.cpp.o" "gcc" "CMakeFiles/cache_lib.dir/src/cache.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cache/build/_deps/googletest-build/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/denis/prog/Ilab2/cache/build/_deps/googletest-src
+# Build directory: /home/denis/prog/Ilab2/cache/build/_deps/googletest-build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("googlemock")
