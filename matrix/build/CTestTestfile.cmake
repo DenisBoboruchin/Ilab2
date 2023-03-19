446ed073b3365bc1e526b4eea742495015459e7c
Denis <denis.boboruchin@gmail.com>
2023-03-20 00:53:36 +0300

    started matrix

--- matrix/build/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/denis/prog/IlabCpp/matrix
-# Build directory: /home/denis/prog/IlabCpp/matrix/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("vector")
--- matrix/build/vector/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/denis/prog/IlabCpp/matrix/vector
-# Build directory: /home/denis/prog/IlabCpp/matrix/build/vector
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
