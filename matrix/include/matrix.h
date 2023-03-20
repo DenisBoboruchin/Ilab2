73213184e4de4602b27ab9c568acad9f1a5bc022
Denis <denis.boboruchin@gmail.com>
2023-03-20 12:06:00 +0300

    started to implement matrix methods

--- matrix/include/matrix.h
+++ matrix/include/matrix.h
@@ -5,12 +5,41 @@
 
 #include "vector.h"
 
-namespace matrix {
+namespace matrix_space {
 
 template <typename T>
 class matrix final {
+public:
+    matrix(int num_rows = 0, int num_cols = 0, const T &value = {});
+
+    int get_num_rows() const;
+    int get_num_cols() const;
+
+private:
+    my_containers::vector<T> data_;
+
+    int num_rows_;
+    int num_cols_;
+};
+
+template <typename T>
+matrix<T>::matrix(int num_rows, int num_cols, const T &value)
+    : data_ {num_rows * num_cols, value}, num_rows_ {num_rows}, num_cols_ {num_cols}
+{
 }
 
-}  // namespace matrix
+template <typename T>
+int matrix<T>::get_num_rows() const
+{
+    return num_rows_;
+}
+
+template <typename T>
+int matrix<T>::get_num_cols() const
+{
+    return num_cols_;
+}
+
+}  // namespace matrix_space
 
 #endif
--- matrix/tests/tests.cpp
+++ matrix/tests/tests.cpp
@@ -2,9 +2,22 @@
 
 #include "matrix.h"
 
+using matrix_space::matrix;
+
 int main()
 {
     testing::InitGoogleTest();
 
     return RUN_ALL_TESTS();
+}
+
+TEST(matrix, constructor)
+{
+    matrix<int> first_matrix {};
+    ASSERT_EQ(first_matrix.get_num_rows(), 0);
+    ASSERT_EQ(first_matrix.get_num_cols(), 0);
+
+    matrix<int> second_matrix(4, 6, 1);
+    ASSERT_EQ(second_matrix.get_num_rows(), 4);
+    ASSERT_EQ(second_matrix.get_num_cols(), 6);
 }
