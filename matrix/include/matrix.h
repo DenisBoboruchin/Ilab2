c8317d66f540df297b7a3fb0f0299cbbdf360b83
Denis <denis.boboruchin@gmail.com>
2023-03-21 16:05:15 +0300

    overloaded square brackets for matrix

--- matrix/include/matrix.h
+++ matrix/include/matrix.h
@@ -9,33 +9,78 @@ namespace matrix_space {
 
 template <typename T>
 class matrix final {
-public:
-    matrix(int num_rows = 0, int num_cols = 0, const T &value = {});
+private:
+    using row_t = typename my_containers::vector<T>;
+    struct proxy_row_t final {
+        T &operator[](size_t num_col);
 
-    int get_num_rows() const;
-    int get_num_cols() const;
+        row_t &row;
+    };
+
+    struct const_proxy_row_t final {
+        const T &operator[](size_t num_col) const;
+
+        const row_t &row;
+    };
+
+public:
+    matrix(size_t num_rows = 0, size_t num_cols = 0, const T &value = {});
+
+    size_t get_num_rows() const;
+    size_t get_num_cols() const;
+
+    proxy_row_t operator[](size_t num_row) &;
+    const const_proxy_row_t operator[](size_t num_row) const &;
 
 private:
-    my_containers::vector<T> data_;
+    my_containers::vector<row_t> data_;
 
-    int num_rows_;
-    int num_cols_;
+    size_t num_rows_;
+    size_t num_cols_;
 };
 
 template <typename T>
-matrix<T>::matrix(int num_rows, int num_cols, const T &value)
-    : data_ {num_rows * num_cols, value}, num_rows_ {num_rows}, num_cols_ {num_cols}
+T &matrix<T>::proxy_row_t::operator[](size_t num_col)
+{
+    return row[num_col];
+}
+
+template <typename T>
+const T &matrix<T>::const_proxy_row_t::operator[](size_t num_col) const
+{
+    return row[num_col];
+}
+
+template <typename T>
+typename matrix<T>::proxy_row_t matrix<T>::operator[](size_t num_row) &
+{
+    row_t &row = data_[num_row];
+    proxy_row_t proxy_row {row};
+    return proxy_row;
+}
+
+template <typename T>
+const typename matrix<T>::const_proxy_row_t matrix<T>::operator[](size_t num_row) const &
+{
+    const row_t &row = data_[num_row];
+    const const_proxy_row_t proxy_row {row};
+    return proxy_row;
+}
+
+template <typename T>
+matrix<T>::matrix(size_t num_rows, size_t num_cols, const T &value)
+    : data_ {num_rows, row_t {num_cols, value}}, num_rows_ {num_rows}, num_cols_ {num_cols}
 {
 }
 
 template <typename T>
-int matrix<T>::get_num_rows() const
+size_t matrix<T>::get_num_rows() const
 {
     return num_rows_;
 }
 
 template <typename T>
-int matrix<T>::get_num_cols() const
+size_t matrix<T>::get_num_cols() const
 {
     return num_cols_;
 }
--- matrix/tests/tests.cpp
+++ matrix/tests/tests.cpp
@@ -21,3 +21,18 @@ TEST(matrix, constructor)
     ASSERT_EQ(second_matrix.get_num_rows(), 4);
     ASSERT_EQ(second_matrix.get_num_cols(), 6);
 }
+
+TEST(matrix, square_brackets)
+{
+    matrix<int> matrix(5, 2, 15);
+    ASSERT_EQ(matrix.get_num_rows(), 5);
+    ASSERT_EQ(matrix.get_num_cols(), 2);
+
+    ASSERT_EQ(matrix[1][1], 15);
+
+    matrix[3][0] = 10;
+    ASSERT_EQ(matrix[3][0], 10);
+
+    const matrix_space::matrix<int> matrix_cnst(10, 10, 7);
+    ASSERT_EQ(matrix_cnst[4][2], 7);
+}
